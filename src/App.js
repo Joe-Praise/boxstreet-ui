b345d96533acadff49a323c297a883fdafcf8ee2
spracious <110341045+spracious@users.noreply.github.com>
2023-10-06 13:49:09 +0100

    verify user update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import LandingPage from './components/Landing Page/LandingPage';
 import Counter from './components/Counter/Counter'
 import Singlemovie from './components/Singlemovie/Singlemovie';
+import SignUp from './components/SignIn-SignOut/signUp';
+import VerifyUser from './components/SignIn-SignOut/verifyUser';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
           <Route path='/' element={<LandingPage/>} />
           <Route path='/movie' element={<Singlemovie/>} />
           <Route path='/counter' element={<Counter />} />
+          <Route path='/reg' element={<SignUp/>} />
+          <Route path='/verify' element={<VerifyUser/>} />
         </Routes>
       </BrowserRouter>
     </div>
