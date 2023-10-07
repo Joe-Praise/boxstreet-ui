a2c0810034ff791b7931adbbe062b15b5e2d26b9
spracious <110341045+spracious@users.noreply.github.com>
2023-10-07 13:14:31 +0100

    removed counter from app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./components/Landing Page/LandingPage";
-import Counter from "./components/Counter/Counter";
 import Singlemovie from "./components/Singlemovie/Singlemovie";
 import SignUp from "./components/SignIn-SignOut/signUp";
 import VerifyUser from "./components/SignIn-SignOut/verifyUser";
@@ -14,7 +13,6 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/movie" element={<Singlemovie />} />
-          <Route path="/counter" element={<Counter />} />
           <Route path="/register" element={<SignUp />} />
           <Route path="/verify" element={<VerifyUser />} />
           <Route path="/movie" element={<SingleMovie />} />
