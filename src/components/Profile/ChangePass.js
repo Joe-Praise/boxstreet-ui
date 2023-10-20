36068e9844ce9450bccaed98c31888da6e38008a
UncleLenny <lengshakjoseph4@gmail.com>
2023-10-20 21:00:49 +0100

    change password styles

--- src/components/Profile/ChangePass.js
+++ src/components/Profile/ChangePass.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../../styles/changepass.css'
 import Navigation from "../Navigation/Navigation";
 
 function ChangePass() {
@@ -11,19 +12,20 @@ function ChangePass() {
           <div class="ccPassform-group">
             <label for="">Email:</label>
             <span></span>
-            <input type="email" name="name" class="inputs" required />
+            <input type="email" name="email" class="inputs" required />
           </div>
           <div class="ccPassform-group">
             <label for="">Current Password:</label>
             <span></span>
-            <input type="password" name="name" class="inputs" required />
+            <input type="password" name="password" class="inputs" required />
           </div>
           <div class="ccPassform-group">
             <label for="">New Password:</label>
             <span></span>
-            <input type="password" name="name" class="inputs" required />
+            <input type="password" name="password" class="inputs" required />
           </div>
           <section>*NOTE: Password should be at least 8 characters</section>
+          
           <div class="ccPassform-group">
             <button class="ccPassform-btn">Submit Password</button>
           </div>
